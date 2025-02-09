dd56de28fe9943445e49a7f17061ed86a6aaf850
rcompton <ryan@ryancompton.net>
2025-02-08 21:05:37 -0800

    Refactor keymap for GMMK2: streamline layers and update navigation functionality

--- keyboards/gmmk/gmmk2/p65/ansi/keymaps/rcompton/keymap.c
+++ keyboards/gmmk/gmmk2/p65/ansi/keymaps/rcompton/keymap.c
@@ -1,70 +1,36 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+#include QMK_KEYBOARD_H
 
- #include QMK_KEYBOARD_H
+enum custom_layers {
+    _BL,
+    _NAV,
+};
 
- enum custom_layers {
-     _BL,
-     _FL,
-     _NAV, // New navigation layer
- };
- 
- // No Tap Dance needed anymore
- 
- const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /* Keymap _BL: Base Layer (Default Layer)
-      */
-     [_BL] = LAYOUT_65_ansi_blocker(
-       QK_GESC,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_DEL,
-       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PGUP,
-       MT(_NAV, KC_LCTL),  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,             KC_PGDN,  // MOD-TAP HERE
-       KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
-       KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,  MO(1),    KC_LEFT,  KC_DOWN,  KC_RGHT
-     ),
- 
-     /* Keymap _FL: Function Layer
-      */
-     [_FL] = LAYOUT_65_ansi_blocker(
-       KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,    _______,  KC_INS,
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_PSCR,  KC_SCRL,  KC_PAUS,   _______,  _______,
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,             _______,
-       _______,  RM_HUEU,  RM_HUED,  RM_SPDD,  RM_SPDU,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MPLY,  KC_MNXT,  _______,             RM_VALU,  KC_HOME,
-       _______,  _______,  _______,                                QK_BOOT,                                _______,  _______,  RM_PREV,   RM_VALD,  RM_NEXT
-     ),
- 
-     /* Keymap _NAV: Navigation Layer (Accessed via LCtrl)
-      */
-     [_NAV] = LAYOUT_65_ansi_blocker(
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
-       _______,  _______,  _______,  _______,  _______,  _______,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  _______,  _______,  _______,          _______,
-       _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          _______,  _______,
-       _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______,  _______
-     )
- };
- 
- bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-   switch (keycode) {
-     case MT(_NAV, KC_LCTL): // Handle the Mod-Tap key
-       if (record->tap.count > 0 && record->event.pressed) {
-         // Tap, send LCtrl
-         tap_code(KC_LCTL);
-         return false;  // Skip default handling
-       }
-       break; // Important: Let the default Mod-Tap handling occur for hold
-   }
-   return true; // Process all other keys normally
- }
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_BL] = LAYOUT_65_ansi_blocker(
+      KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL,
+      KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
+      MT(_NAV, KC_LCTL), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGDN,
+      KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_END,
+      KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT //KC_NO instead of MO(1)
+    ),
+
+    [_NAV] = LAYOUT_65_ansi_blocker(
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MT(_NAV, KC_LCTL):
+      if (record->tap.count > 0 && record->event.pressed) {
+        tap_code(KC_LCTL);
+        return false;
+      }
+      break;
+  }
+  return true;
+}
