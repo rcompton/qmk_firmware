78d6ee51a834242eba847f5e5e8870a565cda10f
rcompton <ryan@ryancompton.net>
2025-03-04 21:56:08 -0800

    Add RGB lighting effects and new light layer to Keychron Q4 keymap

--- keyboards/keychron/q4/ansi/keymaps/rbox_custom/keymap.c
+++ keyboards/keychron/q4/ansi/keymaps/rbox_custom/keymap.c
@@ -3,6 +3,7 @@
 enum custom_layers {
     _BL,
     _NAV,
+    _LIGHT,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -12,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_LBRC, KC_RBRC, KC_BSLS,
         MO(_NAV), KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
         KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LOPT, KC_LCMD,                          KC_SPC,                                    KC_RALT, KC_RGUI, KC_F13, KC_RCTL
+        KC_LCTL, KC_LOPT, KC_LCMD,                          KC_SPC,                                    KC_RALT, KC_RGUI, MO(_LIGHT), KC_RCTL
     ),
 
     /* Navigation Layer (Activated by holding Caps Lock) */
@@ -23,4 +24,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                          _______,                                _______, _______, _______, _______
     ),
+
+    [_LIGHT] = LAYOUT_ansi_61(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_SPI, RGB_SPD, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______,                          _______,                                _______, _______, _______, _______
+    )
 };
